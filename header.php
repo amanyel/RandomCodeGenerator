346d599ddf6acce105b28283f5f8c691329414df
Amany EL Romeh <amanyel@users.noreply.github.com>
2016-07-05 15:43:50 +0300

    header.php

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<title>Random_Code</title>
+
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="screen">
+
+<style type="text/css">
+#wrapper {
+	width:300px;
+	margin:auto;
+	border:2px solid blue;
+	text-align:center;
+	padding:20px;
+	color:blue;
+	background:azure;
+}
+</style>
+
+</head>
+
+
